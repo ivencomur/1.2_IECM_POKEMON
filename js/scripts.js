d7d75ae2cb27df275ba177b23205a293f95676fa
ivencomur <ivancortes@hotmail.com>
2024-11-21 21:41:38 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+let pokemonList= [{name:"Articuno", height: "1.7 m", types: ["ice","flying"]}, 
+    {name:"Charizard", height: "1.7 m", types: ["fire","flying"]}, 
+    {name:"Butterfree", height: "1.1 m", types: ["bug","flying"]}, 
+    {name:"Pidgeot", height: "1.5 m ,", types: ["flying","normal"]}];
